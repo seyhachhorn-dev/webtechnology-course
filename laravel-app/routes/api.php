ee1114bc69ff603f8a54500605d59d26035a5ae3
seyhachhorn-dev <senjihachiro@gmail.com>
2026-07-20 07:26:37 +0700

    added Password reset backend 3.5

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -5,14 +5,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/signup', [AuthController::class, 'signup']);
 Route::post('/signin', [AuthController::class, 'signin']);
-Route::get('/hello', [AuthController::class,'helloWorld']);
-
 Route::get('/verify/email/{id}/{hash}', [AuthController::class, 'verifyEmail'])
     ->middleware('signed')
     ->name('verify.email');
 Route::post('/send/verification-email', [AuthController::class, 'sendVerificationEmail']);
+Route::post('/send/reset-password-email', [AuthController::class, 'sendResetPasswordEmail']);
+Route::post('/set/new-password', [AuthController::class, 'setNewPassword'])->name('set.new-password');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/signout', [AuthController::class, 'signout']);
     Route::get('/verify', [AuthController::class, 'verify']);
-}); 
+});
